fd21f7f503a4b7af97fb4de94ed129267ea1e8d2
Jianming Fan <151937346+Jianming2024@users.noreply.github.com>
2025-09-24 01:20:38 +0200

    finish homepage

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+Console.WriteLine("The app options are: " + System.Text.Json.JsonSerializer.Serialize(appOptions));
 builder.Services.AddScoped<ILibraryService, LibraryService>();
+builder.Services.AddScoped<ISeeder, Seeder>();
 builder.Services.AddDbContext<MyDbContext>(opt =>
 {
     opt.UseNpgsql(appOptions.DbConnectionString );
@@ -15,7 +17,6 @@ builder.Services.AddDbContext<MyDbContext>(opt =>
 builder.Services.AddControllers();
 builder.Services.AddOpenApiDocument(); 
 builder.Services.AddCors();
-
 
 var app = builder.Build();
 app.UseCors(config => config.
@@ -28,4 +29,15 @@ app.MapControllers();
 app.UseOpenApi();
 app.UseSwaggerUi();
 app.GenerateApiClientsFromOpenApi("/../../client/src/generated-ts-client.ts").GetAwaiter().GetResult();
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var seeder = scope.ServiceProvider.GetService<ISeeder>();
+        if (seeder != null)
+        {
+            seeder.Seed();
+        }
+    }
+}
 app.Run();
